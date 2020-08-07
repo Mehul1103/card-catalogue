7cb40be8c738325bd7a78cdc0a032e4cc20709b4
Mehul1103 <mehulsancheti0212@gmail.com>
2020-08-07 11:32:10 -0400

    Updates

---
+++ precache-manifest.ab57667b7765c66c23643e046882df53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ef32784ee6a284129cd17c9edb44d95",
+    "url": "/card-catalogue/index.html"
+  },
+  {
+    "revision": "fb451902afa6ba74c1af",
+    "url": "/card-catalogue/static/css/main.85b247eb.chunk.css"
+  },
+  {
+    "revision": "0fc5e13a98c3602af5a5",
+    "url": "/card-catalogue/static/js/2.cdce6472.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/card-catalogue/static/js/2.cdce6472.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb451902afa6ba74c1af",
+    "url": "/card-catalogue/static/js/main.a5b0afd3.chunk.js"
+  },
+  {
+    "revision": "77dd6bce555d72b1df4f",
+    "url": "/card-catalogue/static/js/runtime-main.13564bf5.js"
+  }
+]);
